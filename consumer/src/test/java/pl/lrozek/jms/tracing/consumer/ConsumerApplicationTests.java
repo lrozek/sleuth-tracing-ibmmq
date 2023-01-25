977e1e778538cc88b2f6c971a4b4272cb424fcea
Łukasz Rożek <lukasz.rozek@consileon.pl>
2023-01-25 17:01:40 +0100

    consumer init

---
+++ consumer/src/test/java/pl/lrozek/jms/tracing/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lrozek.jms.tracing.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
