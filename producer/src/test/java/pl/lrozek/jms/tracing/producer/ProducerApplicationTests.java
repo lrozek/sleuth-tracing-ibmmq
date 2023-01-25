60824024b26d5c13b8f79e3409cf2bda41ec8c18
Łukasz Rożek <lukasz.rozek@consileon.pl>
2023-01-25 16:59:28 +0100

    producer init

---
+++ producer/src/test/java/pl/lrozek/jms/tracing/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lrozek.jms.tracing.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
