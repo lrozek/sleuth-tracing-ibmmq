9b16c1d29fd2df531bad0c60c26ec47d6101dd71
Łukasz Rożek <lukasz.rozek@consileon.pl>
2023-01-26 12:32:03 +0100

    payload as a tag

--- consumer/src/main/java/pl/lrozek/jms/tracing/consumer/listener/MessageListener.java
+++ consumer/src/main/java/pl/lrozek/jms/tracing/consumer/listener/MessageListener.java
@@ -1,5 +1,7 @@
 package pl.lrozek.jms.tracing.consumer.listener;
 
+import org.springframework.cloud.sleuth.annotation.NewSpan;
+import org.springframework.cloud.sleuth.annotation.SpanTag;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class MessageListener {
 
+    @NewSpan
     @JmsListener(destination = "DEV.QUEUE.1")
-    public void receiveMessage(String payload) {
+    public void receiveMessage(@SpanTag("payload") String payload) {
         log.info("received following payload: {}", payload);
     }
 
